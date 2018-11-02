4565da0afcea5f77cef0e3bf9efd592b3864c6d0
Bongki <qhdrl12@gmail.com>
2018-11-02 17:02:54 +0900

    rebase commit
    test2
    
    test3
    
    test4
    
    test5
    
    add root url request mapping string
    
    controller test2
    
    deploy update add gradlew build
    
    add mapping path
    
    update deploy.sh
    
    delete gradlew build
    
    test
    
    update..

---
+++ deploy.sh
@@ -0,0 +1,47 @@
+#!/bin/bsh
+
+LIB_PATH=build/libs
+JAR_FILE=sample-0.0.1-SNAPSHOT.jar
+PID_NAME=application.pid
+
+function stop() {
+    echo " "
+    echo "Stopping process on read pid."
+
+    PID=`pgrep -f $JAR_FILE`
+
+    if [[ -n $PID ]]; then
+        echo $PID
+        `kill $PID`
+#        rm $PID_NAME
+#    else
+#        echo "PROCESS NOT RUNNING"
+    fi
+
+#    if [ -f $PID_NAME ]; then
+#        PID=`cat $PID_NAME`
+#
+#        if [[ -n $PID ]]; then
+#            echo $PID
+#            `kill $PID`
+#            rm $PID_NAME
+#        else
+#            echo "PID IS EMPTY"
+#        fi
+#    else
+#        echo "$PID_NAME not found"
+#    fi
+
+    echo " "
+}
+
+function start() {
+    echo " "
+    echo "java -jar $LIB_PATH/$JAR_FILE"
+    `java -jar $LIB_PATH/$JAR_FILE &`
+    echo " "
+}
+
+stop
+
+#start
