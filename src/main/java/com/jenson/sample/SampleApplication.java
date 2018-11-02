933e2d92afb0200442e4d05eefed2e6ae059a2d1
Bongki <qhdrl12@gmail.com>
2018-11-02 16:50:21 +0900

    add pid

--- src/main/java/com/jenson/sample/SampleApplication.java
+++ src/main/java/com/jenson/sample/SampleApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class SampleApplication {
@@ -11,7 +12,10 @@ public class SampleApplication {
     private static final Logger log = LoggerFactory.getLogger(SampleApplication.class);
 
     public static void main(String[] args) {
-        log.info("test");
-        SpringApplication.run(SampleApplication.class, args);
+        log.info("application started");
+//        SpringApplication.run(SampleApplication.class, args);
+        SpringApplication sa = new SpringApplication(SampleApplication.class);
+        sa.addListeners(new ApplicationPidFileWriter());
+        sa.run(args);
     }
 }
