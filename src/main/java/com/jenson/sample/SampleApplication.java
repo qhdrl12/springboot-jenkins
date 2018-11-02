159dd3b13fbb28959de338ef7fe722ad36dab918
Bongki <qhdrl12@gmail.com>
2018-11-02 16:02:24 +0900

    add log

--- src/main/java/com/jenson/sample/SampleApplication.java
+++ src/main/java/com/jenson/sample/SampleApplication.java
@@ -1,12 +1,17 @@
 package com.jenson.sample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SampleApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(SampleApplication.class);
+
     public static void main(String[] args) {
+        log.info("test");
         SpringApplication.run(SampleApplication.class, args);
     }
 }
