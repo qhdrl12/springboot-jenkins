1cb7dd578f1e50077112e47b179b03fd5113daba
Bongki <qhdrl12@gmail.com>
2018-11-05 09:21:23 +0900

    add root url request mapping string

---
+++ src/main/java/com/jenson/sample/BaseController.java
@@ -0,0 +1,13 @@
+package com.jenson.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseController {
+
+    @GetMapping
+    public String index() {
+        return "Welcome Jenkins Sample Project";
+    }
+}
